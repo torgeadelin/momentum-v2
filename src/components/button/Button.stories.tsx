b7a3144d6a2abacb9d96b36dca56258f0691f6d0
torgeadelin <torgeadelin@gmail.com>
2021-05-07 15:23:27 +0100

    add new story

--- src/components/button/Button.stories.tsx
+++ src/components/button/Button.stories.tsx
@@ -6,6 +6,7 @@ import {Button} from 'components/button'
 
 export const Primary: React.VFC<{}> = () => <Button primary label="Hello"/>;
 export const Secondary: React.VFC<{}> = () => <Button label="Hello"/>;
+export const Text: React.VFC<{}> = () => <Button label="Another text"/>;
 
 const component = {
   title: 'Button',
